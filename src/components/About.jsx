7b1f81623e5af15bd0650a01e0300812ac90b528
nmacenco <nicolasmacenco@gmail.com>
2022-02-06 12:20:00 -0300

    add About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,17 @@
+import c from "../About.module.css";
 
-
-
-function About () {
-    return (
+function About() {
+  return (
+    <div className={`${c.aboutContainer}`} >
+      <div  className={`${c.cardContainer}`}>
         <h1>About</h1>
-    )
+        Application made in the Front End module of Soy Henry bootcamp obtaining
+        the data from Weather API. Technologies used were React, Javascript and
+        CSS modules for its design. With this app you will be able to see the weather of all
+        the cities in the world.
+      </div>
+    </div>
+  );
 }
 
-export default About ; 
+export default About;
